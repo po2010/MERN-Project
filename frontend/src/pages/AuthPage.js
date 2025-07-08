26e65bb42510a800e5e32daae50164dfa7736fff
po2010 <162029253+po2010@users.noreply.github.com>
2025-07-08 12:23:54 +0530

    Update AuthPage.js

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -16,7 +16,7 @@ function AuthPage({ role }) {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post('https://mern-project-backend2-9eul.onrender.com/api/auth/login', {
         username,
         password
       });
